70fbba9def7ab679e8ee52d102c120987b22c8c0
Daniel - [CreeperEvil1227] <110143364+Daniel-CreeperEvil1227@users.noreply.github.com>
2023-03-06 21:25:14 -0300

    Create theme.js

---
+++ themes/github-stats/theme.js
@@ -0,0 +1,11 @@
+export const themes = {
+  darkviolet: {
+    title_color: "#6a00ff",
+    icon_color: "#6a00ff",
+    text_color: "ffffff",
+    bg_color: "211f24",
+    border_color: "#6a00ff",
+  }
+}
+
+export default themes
